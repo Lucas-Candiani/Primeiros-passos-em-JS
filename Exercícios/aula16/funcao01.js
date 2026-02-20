a1b58180587db24854844101db56e9ea8e5218a1
Lucas Candiani <lucascandiani06@gmail.com>
2026-02-20 16:40:04 -0300

    Create funcao01.js

---
+++ Exercícios/aula16/funcao01.js
@@ -0,0 +1,12 @@
+
+function parimp(n){
+    if(n%2 == 0){
+        return `Par`
+    } else {
+        return `Ímpar`
+    }
+}
+
+var res = parimp(16); //Ou somente console.log(parimp(16))
+console.log(res)
+
